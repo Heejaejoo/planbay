c0b37bce232f5f0f06fcc3de6dd4c7706b8b5bae
HeejaeJoo <ymount02@gmail.com>
2016-08-03 21:48:06 +0900

    detail pages not finished

--- project_planbay/public/app/javascripts/controllers.js
+++ project_planbay/public/app/javascripts/controllers.js
@@ -1,12 +1,17 @@
 'use strict';
 angular.module('planBay')
-    .controller('IndexController', ['$scope',function($scope) {
+    .controller('DetailController', ['$scope',function($scope) {
+        $scope.checked = false;
+        $scope.size = '100px';
+
+        $scope.toggle = function() {
+            $scope.checked = !$scope.checked
+        }
 
     }])
-
     .controller('LandingController', ['$scope',function($scope) {
         
-    };
+    }]);
 //example
 //.controller('MenuController', ['$scope', 'menuFactory', function($scope, menuFactory) {
 
